353973d555a43bc690a0f2aeaacc61a811b8abc1
Mauricoseres <mauri.coseres@gmail.com>
2022-09-18 18:34:14 -0300

    clase nueve 2

---
+++ variables.js
@@ -0,0 +1,12 @@
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+
+pais = "mexico";
+continente = "latinoamerica";
+
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_continente);
